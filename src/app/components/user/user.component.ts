0e7e18f2d2e79b1c5813700dec334c774ccccf9f
Eric Olaveson <olavesoneric@gmail.com>
2019-09-25 12:38:49 -0500

    practice

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -11,5 +11,38 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent{
+    //Properties
+    firstName: string;
+    lastName: string;
+    age: number;
+    address;
+
+    //Methods
+    //runs when a component is initialized.
+    //put things in here you want when the page is initialized.
+    //ng on init is where we do ajax calls
+    constructor(){
+        // console.log(this.age);
+        // this.sayHello();
+        // this.hasBirthday();
+        // console.log(this.age)
+        this.firstName = "Eric";
+        this.lastName = "Olaveson";
+        this.age = 30;
+        this.address = {
+            street: 'easy street',
+            city: 'Dallas',
+            zip: '97323'
+        }
+
+    }
+
+    // sayHello(){
+    //     console.log(`Hello ${this.firstName}`)
+    // }
+
+    // hasBirthday(){
+    //     this.age ++;
+    // }
 
 }
