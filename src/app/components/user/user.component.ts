f6db70c49b37263224aeaae64d6e20e4894c7f89
Eric Olaveson <olavesoneric@gmail.com>
2019-09-25 19:56:58 -0500

    working with an interface

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,48 +1,28 @@
 import { Component } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
     selector: 'app-user',
     templateUrl: './user.component.html',
     styleUrls: ['./user.component.css']
-    //template: '<h2>Eric Olaveson</h2>'
-    // styles: [`h2{
-    //     color: blue
-    // }`]
 })
 
 export class UserComponent{
-    //Properties
-    firstName: string;
-    lastName: string;
-    age: number;
-    address;
+    
+    user: User;
 
-    //Methods
-    //runs when a component is initialized.
-    //put things in here you want when the page is initialized.
-    //ng on init is where we do ajax calls
     constructor(){
-        // console.log(this.age);
-        // this.sayHello();
-        // this.hasBirthday();
-        // console.log(this.age)
-        this.firstName = "Eric";
-        this.lastName = "Olaveson";
-        this.age = 30;
-        this.address = {
-            street: 'easy street',
-            city: 'Dallas',
-            zip: '97323'
+        this.user = {
+            firstName : "John",
+            lastName : "Doe",
+            age : 30,
+            address: {
+                street : "Easy Street",
+                city: "Dallas",
+                state: "TX"
+
+            }
         }
-
     }
-
-    // sayHello(){
-    //     console.log(`Hello ${this.firstName}`)
-    // }
-
-    // hasBirthday(){
-    //     this.age ++;
-    // }
-
 }
